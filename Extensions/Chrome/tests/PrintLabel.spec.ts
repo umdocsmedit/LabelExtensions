b0867a6ffb1bffca20c793810dde01d67f153d7d
kevincar <kevincarrolldavis@gmail.com>
2018-12-23 20:39:00 -0700

    No need to test what isn't a part of PrintLabel

--- Extensions/Chrome/tests/PrintLabel.spec.ts
+++ Extensions/Chrome/tests/PrintLabel.spec.ts
@@ -11,21 +11,21 @@
 /// <reference path="../src/types/healthFair.d.ts" />
 /// <reference path="../src/types/patientRecord.d.ts" />
 
-import * as PrintLabel from "../src/PrintLabel"
-import * as assert from 'assert'
+//import * as PrintLabel from "../src/PrintLabel"
+//import * as assert from 'assert'
 
-describe('convertArmToHealthFairName', () => {
-	it('should return the appropriate health fair IDs', () => {
-		// TEST 1
-		let input: number = 1;
-		let expected: Healthfair = "LHHF";
-		let output: Healthfair = PrintLabel.convertArmToHealthFairName(input);
-		assert.equal(output, expected);
+//describe('convertArmToHealthFairName', () => {
+	//it('should return the appropriate health fair IDs', () => {
+		//// TEST 1
+		//let input: number = 1;
+		//let expected: Healthfair = "LHHF";
+		//let output: Healthfair = PrintLabel.convertArmToHealthFairName(input);
+		//assert.equal(output, expected);
 
-		input = 2;
-		expected = "HHF";
-		output = PrintLabel.convertArmToHealthFairName(input);
-		assert.equal(output, expected);
+		//input = 2;
+		//expected = "HHF";
+		//output = PrintLabel.convertArmToHealthFairName(input);
+		//assert.equal(output, expected);
 
-	});
-});
+	//});
+//});
