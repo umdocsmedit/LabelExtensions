03e95c6f1c8915e801417f51f76b13894bdc18e0
kevincar <kevincarrolldavis@gmail.com>
2019-01-10 18:56:12 -0500

    added compression step

--- Extensions/Chrome/scripts/build.sh
+++ Extensions/Chrome/scripts/build.sh
@@ -11,3 +11,5 @@ tsc
 # Browserify
 browserify ./dist/js/PrintLabel.js ./dist/js/popup.js -o ./dist/js/popup.js
 #browserify ./dist/js/PrintLabel.js -o ./dist/js/PrintLabel.js
+
+zip -r UMDOCSMEDITRCP.zip ./dist
