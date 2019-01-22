39122622ad87ea0887916661bbb384748bad0558
kevincar <kevincarrolldavis@gmail.com>
2019-01-21 21:43:26 -0500

    Updated script to get patient data

--- Extensions/Safari/UMDocsMedIT/UMDocsMedIT Extension/JS/src/script.ts
+++ Extensions/Safari/UMDocsMedIT/UMDocsMedIT Extension/JS/src/script.ts
@@ -11,10 +11,33 @@
 
 // ==================================================================
 
+import PatientRecord from "./PatientRecord"
+
 document.addEventListener("DOMContentLoaded", (): void => {
 	safari.extension.dispatchMessage("Hello, World!");
 });
 
-safari.self.addEventListener("message", (): void => {
-	// DO SOMETHING!
+safari.self.addEventListener("message", (event: SafariExtensionMessageEvent): void => {
+	switch(event.name) {
+		case "getPatientData":
+			sendPatientData();
+			break;
+		default:
+			console.log(`${event.name} unrecognized message name`);
+	}
 });
+
+function sendPatientData(): PatientRecord {
+
+	let patientData: PatientRecord;
+
+	try {
+		patientData = new PatientRecord();
+	} catch(e) {
+		patientData = new PatientRecord(true);
+	}
+
+	safari.extension.dispatchMessage("patientData", {data: patientData});
+
+	return patientData;
+}
