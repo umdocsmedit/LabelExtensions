1ff393ca62dd340279280e9637e21895e0ae52a2
kevincar <kevincarrolldavis@gmail.com>
2019-01-22 08:48:08 -0500

    Added a number formatter

---
+++ Extensions/Safari/UMDocsMedIT/UMDocsMedIT Extension/MyNumberFormatter.swift
@@ -0,0 +1,23 @@
+//
+//  NumberFormatter.swift
+//  UMDocsMedIT Extension
+//
+//  Created by Kevin Davis on 1/22/19.
+//  Copyright © 2019 Kevin Davis. All rights reserved.
+//
+
+import Foundation
+
+class MyNumberFormatter : NumberFormatter {
+    override func isPartialStringValid(_ partialString: String, newEditingString newString: AutoreleasingUnsafeMutablePointer<NSString?>?, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
+        if partialString.isEmpty {
+            return true
+        }
+        
+        if partialString.characters.count > 2 {
+            return false
+        }
+        
+        return Int(partialString) != nil
+    }
+}
--- Extensions/Safari/UMDocsMedIT/UMDocsMedIT Extension/SafariExtensionViewController.swift
+++ Extensions/Safari/UMDocsMedIT/UMDocsMedIT Extension/SafariExtensionViewController.swift
@@ -11,19 +11,42 @@ import SafariServices
 class SafariExtensionViewController: SFSafariExtensionViewController {
     
     @IBOutlet weak var patientNameLabel: NSTextField!
+    @IBOutlet weak var numLabelsField: NSTextField!
+    @IBOutlet weak var labsOrderedList: NSPopUpButton!
+    @IBOutlet weak var printButton: NSButton!
+    
+    let labsOrderedOptions: [String] = ["CRC", "Pap Smear", "Lipids"]
     
     static let shared: SafariExtensionViewController = {
         let shared = SafariExtensionViewController()
-        shared.preferredContentSize = NSSize(width:320, height:240)
+        shared.preferredContentSize = NSSize(width:276, height:150)
         return shared;
     }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        numLabelsField.formatter = MyNumberFormatter()
+        DispatchQueue.main.async {
+            self.numLabelsField.resignFirstResponder()
+            self.printButton.becomeFirstResponder()
+        }
+        
+//        self.labsOrderedList.removeAllItems()
+//        self.labsOrderedList.addItems(withTitles: self.labsOrderedOptions)
+//        self.labsOrderedList.selectItem(at: 1)
+//        self.labsOrderedList.synchronizeTitleAndSelectedItem()
+    }
 
     @IBAction func print(_ sender: Any) {
         
     }
     
+    @IBAction func papItemSelected(_ sender: Any) {
+        self.labsOrderedList.selectItem(at: 1)
+        self.labsOrderedList.setTitle(self.labsOrderedList.itemTitle(at: 1))
+    }
+    
     public func setPatientName(_ name: String) {
         self.patientNameLabel.stringValue = "Patient Name: \(name)"
     }
-    
 }
