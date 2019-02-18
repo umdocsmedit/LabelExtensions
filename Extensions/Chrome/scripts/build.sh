12a772b27cd33134e1188ebfdc0ddde74d5947aa
kevincar <kevincarrolldavis@gmail.com>
2019-02-17 21:45:46 -0500

    update the build process for the content script

--- Extensions/Chrome/scripts/build.sh
+++ Extensions/Chrome/scripts/build.sh
@@ -10,7 +10,7 @@ tsc
 
 # Browserify
 browserify ./dist/js/PrintLabel.js ./dist/js/popup.js -o ./dist/js/popup.js
-browserify ./dist/js/PatientRecord.js ./dist/js/contentScript.js -o ./dist/js/contentScript.js
+browserify ./dist/js/PatientRecord.js ./dist/js/gatherData.js ./dist/js/contentScript.js -o ./dist/js/contentScript.js
 browserify ./dist/js/init.js ./dist/js/background.js -o ./dist/js/background.js
 
 zip -r UMDOCSMEDITRCP.zip ./dist
