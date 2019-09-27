6d67f80de7bc38dd2b880d2661577fbe4e2440b6
Kevin Davis <kevincarrolldavis@gmail.com>
2019-09-27 17:28:49 -0400

    Added popupModule to build script

--- Extensions/Chrome/scripts/build.sh
+++ Extensions/Chrome/scripts/build.sh
@@ -9,7 +9,7 @@ cd ../
 tsc
 
 # Browserify
-browserify ./dist/js/PrintLabel.js ./dist/js/popup.js ./dist/js/popupModule.js -o ./dist/js/popup.js
+browserify ./dist/js/PrintLabel.js ./dist/js/popupModule.js ./dist/js/popup.js -o ./dist/js/popup.js
 browserify ./dist/js/PatientRecord.js ./dist/js/gatherData.js ./dist/js/contentScript.js -o ./dist/js/contentScript.js
 browserify ./dist/js/init.js ./dist/js/background.js -o ./dist/js/background.js
 
